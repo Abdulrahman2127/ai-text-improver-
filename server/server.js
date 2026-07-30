baeb9dd9b77cf321a0a45f1a727ed01922d89abc
abodihb22m2-spec <AbodiHB22m2@gmail.com>
2026-07-30 17:26:45 +0300

    Prepare backend for Render deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 })
 
 const app = express()
-const PORT = 5001
+const PORT = process.env.PORT || 5001;
 app.use(cors())
 app.use(express.json())
 
@@ -43,6 +43,6 @@ app.get('/', (req, res) => {
   res.send('Mini MERN Backend 🚀')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(PORT , () => {
+  console.log(`Server running on http://localhost:${PORT }`)
 })
